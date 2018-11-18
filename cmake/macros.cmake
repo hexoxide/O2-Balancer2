96036097f9da2bc7dc021370e4df10834e189111
Dantali0n <corne.lukken@hotmail.com>
2018-11-18 04:21:57 -0500

    Fixed coverage report & increased cmake verbosity

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,7 +1,7 @@
 # enable cxx11 using any version of CMake #
 macro(use_cxx11)
 	if(CMAKE_VERSION VERSION_LESS "3.1")
-		message("CMake < 3.1 setting cxx flags for c++11 manually")
+		message("use_cxx11: CMake < 3.1 setting cxx flags for c++11 manually")
 		if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
 		elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
@@ -20,7 +20,7 @@ endmacro(use_cxx11)
 macro(use_cxx_warning_pedantic)
 	if(MSVC)
 		# Force to always compile with W4
-		message("Found MSVC enabling W4")
+		message("use_cxx_warning_pedantic: Found MSVC enabling W4")
 		if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
 			string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 		else()
@@ -28,8 +28,7 @@ macro(use_cxx_warning_pedantic)
 		endif()
 	elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
 		# Update if necessary
-		
-		message("Found GCC enabling pedantic")
+		message("use_cxx_warning_pedantic: Found GCC enabling pedantic")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Werror")
 	endif()
 endmacro(use_cxx_warning_pedantic)
