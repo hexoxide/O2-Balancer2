57b058a830a3f94653e003e0e1851e8ae13a40cb
Geoffrey v D <geoffrey.v.d@live.nl>
2019-01-16 16:26:02 +0100

    idk

--- flp/src/main.cxx
+++ flp/src/main.cxx
@@ -6,7 +6,7 @@ namespace bpo = boost::program_options;
 void addCustomOptions(bpo::options_description& options)
 {
     options.add_options()
-    	("bytes-per-message", bpo::value<uint64_t>()->default_value(20480), "Number of bytes to transmit per message");
+    	("bytes-per-message", bpo::value<uint64_t>()->default_value(232000), "Number of bytes to transmit per message");
 }
 
 FairMQDevicePtr getDevice(const FairMQProgOptions& /*config*/)
